a88f75df0c0a6b1ee6ed77c102331335c92b3b33
JWMin556 <117150131+JWMin556@users.noreply.github.com>
2024-01-10 23:54:58 +0900

    paragraph 3개를 별도의 styled-component로 옮김

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import * as P from '../styled-components/IntroduceParagraph'  //메인화면의 세 문장을 스타일드 컴포넌트로 따로 분리시켜서 사용하고자 이렇게 import를 했습니다. 
 
 //Home.jsx는 메인화면을 위한 컴포넌트입니다. 즉, 메인페이지라고 생각하시면 될 것 같습니다. 
 
@@ -13,41 +14,6 @@ const HomeWrap = styled.div `
 const HomeLeftWrap = styled.div `
   display: block;
 `;  //메인화면의  왼쪽 부분 즉, 전시회 사진을 제외한 나머지 부분을 감싸주기 위한 스타일드 컴포넌트입니다. 
-
-const FirstParagraph = styled.p `
-  color: #595959;
-  font-size: 14px;
-  font-family: Pretendard;
-  font-weight: 500;
-  line-height: 18.63px; 
-  letter-spacing: 0.49px;
-  word-wrap: break-word;
-  width: 116px;
-  height: 19px;
-`;  //첫 문장인 Story community를 위한 스타일드 컴포넌트입니다. 
-
-const SecondParagraph = styled.h2 `
-  color: #5D5D5D;
-  font-size: 40px;
-  font-family: Pretendard;
-  font-weight: 500;
-  line-height: 53.23px;
-  word-wrap: break-word;
-  width: 405px;
-  height: 41px;
-`; //두 번쨰 문장인 '나만의 문화일기'를 위한 스타일드 컴포넌트입니다. 
-
-const ThirdParagraph = styled.h1 `
-  color: black;
-  font-size: 80px;
-  font-family: Pretendard;
-  font-weight: 700;
-  line-height: 106.46px;
-  letter-spacing: 2.80px;
-  word-wrap: break-word;
-  width: 393px;
-  height: 77px;
-`;  //세 번째 문장인 ARTORY를 위한 스타일드 컴포넌트입니다. 
 
 const LogInBtn = styled.img `
   margin-top: 100px;
@@ -65,9 +31,9 @@ export default function Home() {
   return (
     <HomeWrap>
       <HomeLeftWrap>
-        <FirstParagraph>Story community</FirstParagraph>
-        <SecondParagraph>나만의 문화일기</SecondParagraph>
-        <ThirdParagraph>ARTORY</ThirdParagraph>
+        <P.FirstParagraph>Story community</P.FirstParagraph>
+        <P.SecondParagraph>나만의 문화일기</P.SecondParagraph>
+        <P.ThirdParagraph>ARTORY</P.ThirdParagraph>
         <Link to="/login"><LogInBtn src='/img/button.png'></LogInBtn></Link> {/*현재는 이 버튼을 누르면 로그인 페이지로 넘어가도록 만들었습니다. 주연씨는 LogIn.jsx에서 바로 로그인 화면을 작업해주시면 될 것 같습니다. */}
       </HomeLeftWrap>
       <ExhibitImg src='/img/imagearbitary.png'></ExhibitImg>
