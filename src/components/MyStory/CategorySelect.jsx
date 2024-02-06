3da3971087f1504282f54fa4b3a6da4c1bbac7cc
JoEunHyang <click2jeh@naver.com>
2024-02-06 20:13:15 +0900

    arbitarymaster 가져오기

--- src/components/MyStory/CategorySelect.jsx
+++ src/components/MyStory/CategorySelect.jsx
@@ -8,7 +8,7 @@ const genres = [
   'PICTURE',
   'SPECIAL_EXHIBITION',
   'SCULPTURE',
-  'PLANEXHIBITION',
+  'PLAN_EXHIBITION',
   'INSTALLATION_ART',
   'PAINTING',
   'ARTIST_EXHIBITION',
@@ -21,10 +21,10 @@ export default function CategorySelect({
   blackBox,
   setGenre1,
   setGenre2,
-  setGenre3
+  setGenre3,
 }) {
   const [selectedTopics, setSelectedTopics] = useState([]);
-  const [selectedIndex, setSelectedIndex] = useState([]);    
+  const [selectedIndex, setSelectedIndex] = useState([]);
   const [prevIdx, setPrevIdx] = useState();
   //const [prevIdx, setPrevIdx] = useState(0);
   const toggleBox = (index) => {
@@ -32,61 +32,61 @@ export default function CategorySelect({
       setBox((prevBox) => {
         const newBox = [...prevBox];
         //주제가 이미 선택되어 있을 때
-        if(selectedTopics.includes(greyBox[index].props.children) ) 
-        {
+        if (selectedTopics.includes(greyBox[index].props.children)) {
           if (prevIdx !== index) {
             //console.log(1);
             newBox[index] = greyBox[index]; //기존에 선택된 박스 색 원래대로 (블랙->그레이), 3개의 주제를 모두 선택했을 경우에
-            const index1 = selectedTopics.indexOf(greyBox[index].props.children)
-            selectedTopics.splice(index1,1) //삭제
-            selectedIndex.splice(index1,1)
-         }
-         else if(prevIdx===index) 
-         {
-          newBox[prevIdx] = greyBox[prevIdx]; //기존에 선택된 박스 색 원래대로 (블랙->그레이), 3개의 주제를 모두 선택했을 경우에
-          const index1 = selectedTopics.indexOf(greyBox[index].props.children)
-          //console.log("인덱스",index1)
-          selectedTopics.splice(index1,1)
-          selectedIndex.splice(index1,1)
-         }
+            const index1 = selectedTopics.indexOf(
+              greyBox[index].props.children
+            );
+            selectedTopics.splice(index1, 1); //삭제
+            selectedIndex.splice(index1, 1);
+          } else if (prevIdx === index) {
+            newBox[prevIdx] = greyBox[prevIdx]; //기존에 선택된 박스 색 원래대로 (블랙->그레이), 3개의 주제를 모두 선택했을 경우에
+            const index1 = selectedTopics.indexOf(
+              greyBox[index].props.children
+            );
+            //console.log("인덱스",index1)
+            selectedTopics.splice(index1, 1);
+            selectedIndex.splice(index1, 1);
+          }
         }
         //주제가 선택되어 있지 않고, 선택된 주제의 개수가 3개 미만일 때
         else if (selectedTopics.length < 3) {
           //console.log(2);
           setPrevIdx(index); // 누른 박스의 인덱스로 변경
           newBox[index] = // 누른 박스의 색깔 변경 (그레이 -> 블랙)
-            newBox[index] === greyBox[index]
-              ? blackBox[index]
-              : greyBox[index]; // Toggle between grey and black
+            newBox[index] === greyBox[index] ? blackBox[index] : greyBox[index]; // Toggle between grey and black
           setSelectedTopics((prevSelectedTopics) => [
             ...prevSelectedTopics,
             greyBox[index].props.children,
           ]);
           selectedIndex.push(index);
-        }
-        else{ //이미 3개의 주제 선택 
+        } else {
+          //이미 3개의 주제 선택
           alert('최대 3개의 주제만 선택할 수 있습니다.');
         }
 
-      // 이 부분에서 선택된 인덱스를 상위 컴포넌트로 전달합니다.
-        onSelect(newBox[index].props.children === blackBox[index].props.children ? index : -1);
+        // 이 부분에서 선택된 인덱스를 상위 컴포넌트로 전달합니다.
+        onSelect(
+          newBox[index].props.children === blackBox[index].props.children
+            ? index
+            : -1
+        );
         return newBox;
       });
+    }
   };
-}
-console.log("선택된 주제들 인덱스",selectedIndex);
-setGenre1(genres[selectedIndex[0]]);
-setGenre2(genres[selectedIndex[1]]);
-setGenre3(genres[selectedIndex[2]]);
+  console.log('선택된 주제들 인덱스', selectedIndex);
+  setGenre1(genres[selectedIndex[0]]);
+  setGenre2(genres[selectedIndex[1]]);
+  setGenre3(genres[selectedIndex[2]]);
 
   return (
     <WrapBox>
       {box.map((src, index) => (
-        <Box
-        key={index}
-        onClick={() => toggleBox(index)}
-        >
-        {box[index]}
+        <Box key={index} onClick={() => toggleBox(index)}>
+          {box[index]}
         </Box>
       ))}
     </WrapBox>
@@ -94,10 +94,10 @@ setGenre3(genres[selectedIndex[2]]);
 }
 
 const Box = styled.span`
-    margin : 0.5%;
+  margin: 0.5%;
 `;
 
 const WrapBox = styled.div`
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 `;
--- src/pages/Record.jsx
+++ src/pages/Record.jsx
@@ -237,7 +237,7 @@
 //   );
 // }
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import WritingStory from '../components/MyStory/WritingStory';
@@ -250,10 +250,10 @@ import { progressSaveApi } from '../components/API/StorySave';
 import Banner from '../components/Story/Banner';
 
 const Container = styled.div`
-    display : flex;
-    flex-direction : column;
-    justify-content : center;
-    align-items : center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 // const WrapBanner = styled.div`
 //     width : 100%;
@@ -277,207 +277,228 @@ const Container = styled.div`
 //     }
 // `;
 const Title = styled.div`
-    position : relative;
-    bottom :250px;
-    left : 34%;
-    color : #fff;
-    width : 12em;
-    margin-bottom : 2%;
-    text-align : center;
-    font-size : 40px;
-    font-weight : bold;
-    font-family: 'Pretendard';
+  position: relative;
+  bottom: 250px;
+  left: 34%;
+  color: #fff;
+  width: 12em;
+  margin-bottom: 2%;
+  text-align: center;
+  font-size: 40px;
+  font-weight: bold;
+  font-family: 'Pretendard';
 `;
 
 const StoryRecord = styled.div`
-    //border : 1px solid #000;
-    width : 800px;
-    display : flex;
-    flex-direction : column;
-    justify-content : start;
-    align-items : center;
+  //border : 1px solid #000;
+  width: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
 `;
 const Story = styled.div`
-    //border : 1px solid red;
-    width : 100%;
-    font-size : 30px;
-    font-weight : bold;
-    font-family: 'Pretendard';
-    margin-top : 10%;
+  //border : 1px solid red;
+  width: 100%;
+  font-size: 30px;
+  font-weight: bold;
+  font-family: 'Pretendard';
+  margin-top: 10%;
 `;
 const Story2 = styled.div`
-    width :100%;
-    margin-top : 0.5%;
-    font-family: 'Pretendard';
-    font-size : 14px;
-    color : #979797;
+  width: 100%;
+  margin-top: 0.5%;
+  font-family: 'Pretendard';
+  font-size: 14px;
+  color: #979797;
 `;
 const OpenSelectButton = styled.div`
-    //border : 1px solid red;
-    width : 100%;
-    //text-align : end;
-    margin-bottom : 5%;
-    margin-top : 1%;
-    display : flex;
-    justify-content : end;
-    & button {
-        background-color : #f3f3f3;
-        color : #979797;
-        font-family: 'Pretendard';
-        font-size : 14px;
-        margin-left : 1%;
-        width : 83px;
-        height : 30px;
-        border : none;
-        &:focus{
-            background-color : #000;
-            color : #fff;
-        }
+  //border : 1px solid red;
+  width: 100%;
+  //text-align : end;
+  margin-bottom: 5%;
+  margin-top: 1%;
+  display: flex;
+  justify-content: end;
+  & button {
+    background-color: #f3f3f3;
+    color: #979797;
+    font-family: 'Pretendard';
+    font-size: 14px;
+    margin-left: 1%;
+    width: 83px;
+    height: 30px;
+    border: none;
+    &:focus {
+      background-color: #000;
+      color: #fff;
     }
+  }
 `;
 const WrapSaveButton = styled.div`
-    display : flex;
-    justify-content : space-between;
-    width : 19%;
-    margin-bottom : 10%;
-    & button {
-        width : 123px;
-        height : 40px;
-        background-color : #242526;
-        color : #fff;
-        border : none;
-    }
+  display: flex;
+  justify-content: space-between;
+  width: 19%;
+  margin-bottom: 10%;
+  & button {
+    width: 123px;
+    height: 40px;
+    background-color: #242526;
+    color: #fff;
+    border: none;
+  }
 `;
 export default function Record(props) {
-    const navigate = useNavigate();
-    const today = new Date();
-    const getYear = moment(today).format('YYYY');
-    const getMonth = moment(today).format('MM')
-    const getDate = moment(today).format('DD')
+  const navigate = useNavigate();
+  const today = new Date();
+  const getYear = moment(today).format('YYYY');
+  const getMonth = moment(today).format('MM');
+  const getDate = moment(today).format('DD');
 
-    const { state } = useLocation();
-    console.log("레코드 페이지 ",state.userStoryData);
-    const userStoryData = state.userStoryData
-    //console.log("userStoryData",userStoryData)
-    const [data, setData] = useState(''); //스토리 내용
-    const [title,setTitle] = useState(''); //제목
-    const [viewingTime, setViewingTime] = useState(''); //시간
-    const [satisfactionLevel,setSatisfactionLevel] = useState(''); //만족도
-    const [weather,setWeather] = useState(''); //날씨
-    const [companion,setCompanion] = useState(''); //동반인
-    const [genre1,setGenre1] = useState(''); //장르1
-    const [genre2,setGenre2] = useState(''); //장르2
-    const [genre3,setGenre3] = useState(''); //장르3
-    const [isOpen,setIsOpen] = useState(true); //공개여부
-    const [year,setYear] = useState(getYear); //년
-    const [month,setMonth] = useState(getMonth); //월
-    const [date,setDate] = useState(getDate); //일
+  const { state } = useLocation();
+  console.log('레코드 페이지 ', state.userStoryData);
+  const userStoryData = state.userStoryData;
+  //console.log("userStoryData",userStoryData)
+  const [data, setData] = useState(''); //스토리 내용
+  const [title, setTitle] = useState(''); //제목
+  const [viewingTime, setViewingTime] = useState(''); //시간
+  const [satisfactionLevel, setSatisfactionLevel] = useState(''); //만족도
+  const [weather, setWeather] = useState(''); //날씨
+  const [companion, setCompanion] = useState(''); //동반인
+  const [genre1, setGenre1] = useState(''); //장르1
+  const [genre2, setGenre2] = useState(''); //장르2
+  const [genre3, setGenre3] = useState(''); //장르3
+  const [isOpen, setIsOpen] = useState(true); //공개여부
+  const [year, setYear] = useState(getYear); //년
+  const [month, setMonth] = useState(getMonth); //월
+  const [date, setDate] = useState(getDate); //일
 
-    const id = state.item.exhibitionId
-    const exhibitionTitle = state.item.exhibitionTitle
-    // console.log("제목: ",title)
-    // console.log("시간: ",viewingTime)
-    // console.log("동반인: ",companion)
-    // console.log("장르1: ",genre1)
-    // console.log("장르2: ",genre2)
-    // console.log("장르3: ",genre3)
-    // console.log("만족도",satisfactionLevel)
-    // console.log("날씨",weather)
-    // console.log("내용",data)
-    // console.log("공개여부",isOpen)
-    // console.log("년",year)
-    // console.log("월",month)
-    // console.log("일",date)
-    const markEvent=[
-        state.item.exhibitionTitle,
+  const id = state.item.exhibitionId;
+  const exhibitionTitle = state.item.exhibitionTitle;
+  // console.log("제목: ",title)
+  // console.log("시간: ",viewingTime)
+  // console.log("동반인: ",companion)
+  // console.log("장르1: ",genre1)
+  // console.log("장르2: ",genre2)
+  // console.log("장르3: ",genre3)
+  // console.log("만족도",satisfactionLevel)
+  // console.log("날씨",weather)
+  // console.log("내용",data)
+  // console.log("공개여부",isOpen)
+  // console.log("년",year)
+  // console.log("월",month)
+  // console.log("일",date)
+  const markEvent = [state.item.exhibitionTitle, year, month, date];
+  //저장 버튼 클릭
+  const handleSubmit = async () => {
+    // console.log('data.exhibitionTitle', userStoryData[0].exhibitionTitle);
+    console.log('exhibitionTitle', exhibitionTitle);
+    for (let i = 0; i < userStoryData.length; i++) {
+      if (userStoryData[i].exhibitionTitle === exhibitionTitle) {
+        alert('동일한 전시가 있습니다.');
+        break;
+      }
+    }
+    //await createStory(id,data,title,viewingTime,companion,genre1,genre2,genre3,satisfactionLevel,weather,isOpen,year,month,date);
+    alert('스토리가 저장되었습니다');
+    //navigate(`/mystory`, { state: { markEvent }
+    try {
+      await createStory(
+        id,
+        data,
+        title,
+        viewingTime,
+        companion,
+        genre1,
+        genre2,
+        genre3,
+        satisfactionLevel,
+        weather,
+        isOpen,
         year,
         month,
         date
-    ]
-    //저장 버튼 클릭 
-    const handleSubmit = async () =>{
-        console.log("data.exhibitionTitle",userStoryData[0].exhibitionTitle)
-        console.log("exhibitionTitle",exhibitionTitle)
-        for (let i=0 ; i<userStoryData.length ; i++){
-            if(userStoryData[i].exhibitionTitle === exhibitionTitle)
-            {
-                alert("동일한 전시가 있습니다.")
-                break;
-            }
-        }
-            //await createStory(id,data,title,viewingTime,companion,genre1,genre2,genre3,satisfactionLevel,weather,isOpen,year,month,date);
-            alert("스토리가 저장되었습니다")
-            //navigate(`/mystory`, { state: { markEvent } 
-        try {
-            await createStory(id,data,title,viewingTime,companion,genre1,genre2,genre3,satisfactionLevel,weather,isOpen,year,month,date);
-            alert("스토리가 저장되었습니다")
-            navigate(`/mystory`, { state: { markEvent } }) 
-        } catch (error) {
-            console.log(error.response.data);
-        }
+      );
+      alert('스토리가 저장되었습니다');
+      navigate(`/mystory`, { state: { markEvent } });
+    } catch (error) {
+      console.log(error.response.data);
     }
+  };
 
-      //임시저장 버튼 클릭 
-    const handleProgressSubmit = async() =>{
-        try{
-            await progressSaveApi(id,data,title,viewingTime,companion,genre1,genre2,genre3,satisfactionLevel,weather,isOpen,year,month,date);
-            alert("스토리가 임시저장 되었습니다")
-            //navigate(`/mystory`, { state: { markEvent } }) 
-        }catch (error) {
-            console.log(error.response.data);
-        }
+  //임시저장 버튼 클릭
+  const handleProgressSubmit = async () => {
+    try {
+      await progressSaveApi(
+        id,
+        data,
+        title,
+        viewingTime,
+        companion,
+        genre1,
+        genre2,
+        genre3,
+        satisfactionLevel,
+        weather,
+        isOpen,
+        year,
+        month,
+        date
+      );
+      alert('스토리가 임시저장 되었습니다');
+      //navigate(`/mystory`, { state: { markEvent } })
+    } catch (error) {
+      console.log(error.response.data);
     }
-    //   const ClickSaveButton =()=>{
-    //     //alert("스토리가 저장되었습니다")
-    //     navigate(`/mystory`, { state: { markEvent } }) 
-    //   }
-    const ClickedOpen =()=>
-    {
-        setIsOpen(true)
-    }
-    const ClickClosed =()=>
-    {
-        setIsOpen(false)
-    }
+  };
+  //   const ClickSaveButton =()=>{
+  //     //alert("스토리가 저장되었습니다")
+  //     navigate(`/mystory`, { state: { markEvent } })
+  //   }
+  const ClickedOpen = () => {
+    setIsOpen(true);
+  };
+  const ClickClosed = () => {
+    setIsOpen(false);
+  };
 
-
-return (
+  return (
     <div>
-        <Banner
+      <Banner
         image={state.item.exhibitionImage}
-        title={state.item.exhibitionTitle}/>
-        <Container>
+        title={state.item.exhibitionTitle}
+      />
+      <Container>
         <StoryRecord>
-            <Story>스토리 기록</Story>
-            <Story2>나만의 스토리를 작성해보세요</Story2>
-            <StoryTitle SetTitle={setTitle}/>
-            <OpenSelectButton>
-                <button onClick={ClickedOpen}>공개</button>
-                <button onClick={ClickClosed}>비공개</button>
-            </OpenSelectButton>
-            <TodayExhibition 
-                setViewingTime={setViewingTime}
-                setSatisfactionLevel={setSatisfactionLevel}
-                setWeatherLevel={setWeather}
-                setCompanion={setCompanion}
-                setGenre1={setGenre1}
-                setGenre2={setGenre2}
-                setGenre3={setGenre3}
-                year={year}
-                month={month}
-                date={date}
-                setYear = {setYear}
-                setMonth = {setMonth}
-                setDate = {setDate}
-                />
-            <WritingStory data={data} setData={setData}/>
+          <Story>스토리 기록</Story>
+          <Story2>나만의 스토리를 작성해보세요</Story2>
+          <StoryTitle SetTitle={setTitle} />
+          <OpenSelectButton>
+            <button onClick={ClickedOpen}>공개</button>
+            <button onClick={ClickClosed}>비공개</button>
+          </OpenSelectButton>
+          <TodayExhibition
+            setViewingTime={setViewingTime}
+            setSatisfactionLevel={setSatisfactionLevel}
+            setWeatherLevel={setWeather}
+            setCompanion={setCompanion}
+            setGenre1={setGenre1}
+            setGenre2={setGenre2}
+            setGenre3={setGenre3}
+            year={year}
+            month={month}
+            date={date}
+            setYear={setYear}
+            setMonth={setMonth}
+            setDate={setDate}
+          />
+          <WritingStory data={data} setData={setData} />
         </StoryRecord>
         <WrapSaveButton>
-            <button onClick={handleProgressSubmit}>임시저장</button>
-            <button onClick={handleSubmit}>저장</button>
+          <button onClick={handleProgressSubmit}>임시저장</button>
+          <button onClick={handleSubmit}>저장</button>
         </WrapSaveButton>
-        </Container>
+      </Container>
     </div>
-
-)
+  );
 }
