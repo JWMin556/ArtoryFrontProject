72c9598b939bac31a82756704c0d1b7f73051ab8
JWMin556 <117150131+JWMin556@users.noreply.github.com>
2024-02-11 23:08:27 +0900

    2월 11일 회원가입 메일보내기 추가

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import './signup.css';
+import '../css/signup.css';
 import StyledButton from '../styled-components/StyledButton';
+import emailjs from 'emailjs-com';
 import axios from 'axios';
 import Modal from '../components/SignUp/Modal ';
 import Modal2 from '../components/SignUp/Modal2';
@@ -112,6 +113,36 @@ export default function SignUp() {
     // } else {
     //   setEmailValid(false);
     // }
+  };
+
+  //Emailjs시험용
+  const [isEmailSent, setIsEmailSent] = useState(false);
+    const sendVerificationEmail = () => {
+      const templateParams = {
+        to_email: 'gtj556@naver.com',// 수신 이메일 ex) test@test.gmail.com,
+        from_name : 'test',
+        message: '인증되었습니다.'
+    };
+
+    emailjs
+      .send(
+        'test-service', // 서비스 ID
+        'test-templete', // 템플릿 ID
+        templateParams,
+        'qsos29YmuOaN1CNcl', // public-key
+      )
+      .then((response) => {
+        console.log('이메일이 성공적으로 보내졌습니다:', response);
+        setIsEmailSent(true);
+      })
+      .catch((error) => {
+        console.error('이메일 보내기 실패:', error);
+        // 이메일 전송 실패 처리 로직 추가
+      });
+  };
+
+  const handleVerification = () => {
+    sendVerificationEmail();
   };
 
   const onChangePassword = (e) => {
@@ -231,6 +262,14 @@ export default function SignUp() {
               value={email}
               onChange={onChangeEmail}
             />
+            {/* {isEmailSent ? (
+                <div>
+                    인증 이메일이 성공적으로 발송되었습니다. 이메일을
+                    확인해주세요!
+                </div>
+            ) : (
+                <button onClick={handleVerification}>인증</button>
+            )} */}
             <StyledButton disabled={notAllow} color="#9BA0AE" height="25px" width="20%" fontSize="13px">인증하기</StyledButton>
           </InputWrap>
         </SuperTitle>
